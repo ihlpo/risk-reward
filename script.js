1737a7ed621c21595861ee641f21aa4bbb12c69c
Ivan <21068310+ihlpo@users.noreply.github.com>
2021-11-29 21:45:49 +1100

    Added calculation functions

--- script.js
+++ script.js
@@ -80,18 +80,71 @@ function calculate_tax(){
 
 }
 
+function convert_percentage(value){
+   return `${(value * 100).toFixed(2)}%`
+}
+
 function choose_short(){
    const button1 = document.getElementById('long');
-   button1.style.color = "grey";
+   button1.style.color;
    const button2 = document.getElementById('short');
-   button2.style.color = "red";
+   button2.style.color;
+   isLong = false;
 }
 
 function choose_long(){
    const button1 = document.getElementById('short');
-   button1.style.color = "grey";
+   button1.classList;
    const button2 = document.getElementById('long');
-   button2.style.color = "green";
+   button2.style.color;
+   isLong = true;
 }
+
+function calculate_rrr(){
+   isLong ? calculate_long() : calculate_short();
+
+}
+
+function calculate_long(){
+   console.log("long")
+   const entryValue = document.getElementById('entry').value;
+   const stopValue = document.getElementById('stop').value;
+   const targetValue = document.getElementById('target').value;
+   const rrrOutput = document.getElementById('rrr');
+   const targetPNLOutput = document.getElementById('target-pnl');
+   const stopPNLOutput = document.getElementById('stop-pnl');
+
+   rrr = (targetValue - entryValue) / ((entryValue - stopValue) < 0 ? 1 : entryValue - stopValue);
+   targetPNL = (targetValue - entryValue) / entryValue;
+   stopPNL = (entryValue - stopValue) / entryValue;
+
+   rrrOutput.innerHTML = rrr;
+   targetPNLOutput.innerHTML = convert_percentage(targetPNL);
+   stopPNLOutput.innerHTML = convert_percentage(stopPNL);
+   return;
+}
+
+function calculate_short(){
+   console.log("short")
+   const entryValue = document.getElementById('entry').value;
+   const stopValue = document.getElementById('stop').value;
+   const targetValue = document.getElementById('target').value;
+   const rrrOutput = document.getElementById('rrr');
+   const targetPNLOutput = document.getElementById('target-pnl');
+   const stopPNLOutput = document.getElementById('stop-pnl');
+
+   rrr = (stopValue - entryValue) / ((entryValue - targetValue) < 0 ? 1 : entryValue - targetValue);
+   targetPNL = (entryValue - targetValue) / entryValue;
+   stopPNL = (stopValue - entryValue) / entryValue;
+
+   rrrOutput.innerHTML = rrr;
+   targetPNLOutput.innerHTML = convert_percentage(targetPNL);
+   stopPNLOutput.innerHTML = convert_percentage(stopPNL);
+}
+document.getElementById("entry").addEventListener("input", calculate_rrr);
+document.getElementById("stop").addEventListener("input", calculate_rrr);
+document.getElementById("target").addEventListener("input", calculate_rrr);
 document.getElementById("long").addEventListener("mousedown", choose_long);
+document.getElementById("long").addEventListener("mousedown", calculate_rrr);
 document.getElementById("short").addEventListener("mousedown", choose_short);
+document.getElementById("short").addEventListener("mousedown", calculate_rrr);
